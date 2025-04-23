3b58b3ff97410449e610439d5a5f4e2d449574ad
weidejunde <weidejunde@gmail.com>
2025-04-24 06:24:14 +0700

    Add files via upload

---
+++ header.js
@@ -0,0 +1,14 @@
+// File JavaScript này sẽ chứa nội dung HTML dưới dạng chuỗi
+const headerHTML = `
+  <header>
+    <nav>
+      <ul>
+        <li><a href="/">Trang chủ</a></li>
+        <li><a href="/about">Giới thiệu</a></li>
+        <li><a href="/contact">Liên hệ</a></li>
+      </ul>
+    </nav>
+  </header>
+`;
+
+export default headerHTML;
